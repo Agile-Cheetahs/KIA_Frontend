b3b1ce29c5f107cc56b54ca36509b68100af2e2a
James Johnson <jamesj4109@gmail.com>
2024-03-01 19:15:43 -0700

    Baselining the project.

---
+++ src/pages/Home.tsx
@@ -0,0 +1,62 @@
+import MessageListItem from '../components/MessageListItem';
+import { useState } from 'react';
+import { Message, getMessages } from '../data/messages';
+import {
+  IonContent,
+  IonHeader,
+  IonButton,
+  IonInput,
+  IonList,
+  IonPage,
+  IonRefresher,
+  IonRefresherContent,
+  IonTitle,
+  IonToolbar,
+  useIonViewWillEnter
+} from '@ionic/react';
+import './Home.css';
+
+const Home: React.FC = () => {
+
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  useIonViewWillEnter(() => {
+    const msgs = getMessages();
+    setMessages(msgs);
+  });
+
+  const refresh = (e: CustomEvent) => {
+    setTimeout(() => {
+      e.detail.complete();
+    }, 3000);
+  };
+
+  return (
+    <IonPage id="home-page">
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Kitchen App</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <IonRefresher slot="fixed" onIonRefresh={refresh}>
+          <IonRefresherContent></IonRefresherContent>
+        </IonRefresher>
+
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">
+              Kitchen
+            </IonTitle>
+          </IonToolbar>
+        </IonHeader>        
+        <IonInput placeholder="User Name"></IonInput>
+        <IonButton>Login</IonButton>
+        
+      </IonContent>
+    </IonPage>
+  );
+};
+
+//{messages.map(m => <MessageListItem key={m.id} message={m} />)} 
+export default Home;
