ac802b6cf6920514e468e9a0d93f59959c1ea7d1
Devesh <dsawant@arizona.edu>
2024-04-21 10:57:26 -0700

    Formatted shopping list items - added api for view but still need to integrate res of the APIs

--- src/helper/APIRequest.tsx
+++ src/helper/APIRequest.tsx
@@ -8,15 +8,15 @@ Helper functions for performing API calls to backend server
 */
 
 export async function register(requestObject: Object) {
-    
+
     let response;
     try {
 
-        const endpoint =   '/api/account/signup';
+        const endpoint = '/api/account/signup';
 
         //(Capacitor.getPlatform() === "android" ? 'https://danibazi9.pythonanywhere.com/api/account/login'
-        
-         response = await fetch(BASE_API_URL + endpoint, {
+
+        response = await fetch(BASE_API_URL + endpoint, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
@@ -36,11 +36,11 @@ export async function register(requestObject: Object) {
         return data;
 
     }
-    catch (error : any) {
+    catch (error: any) {
         // Log any errors to the console
         console.error("There has been a problem with your fetch operation:", error);
         const data = await response.json();
-        return { response: "failed", status: error.msg, data: data};
+        return { response: "failed", status: error.msg, data: data };
     }
 }
 
@@ -48,10 +48,10 @@ export async function login(requestObject: Object) {
     let response;
     try {
 
-        const endpoint =  '/api/account/login';
+        const endpoint = '/api/account/login';
 
         //(Capacitor.getPlatform() === "android" ? 'https://danibazi9.pythonanywhere.com/api/account/login'
-         response = await fetch(BASE_API_URL + endpoint, {
+        response = await fetch(BASE_API_URL + endpoint, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
@@ -68,15 +68,15 @@ export async function login(requestObject: Object) {
         const data = await response.json();
 
         // Return the data
-        return {...data, response: "successful"};
+        return { ...data, response: "successful" };
 
     }
-    catch (error : any) {
+    catch (error: any) {
         // Log any errors to the console
         console.error("There has been a problem with your fetch operation:", error);
         const data = await response.json();
 
-        return { response: "failed", status: error.msg, data: data};
+        return { response: "failed", status: error.msg, data: data };
     }
 }
 
@@ -87,14 +87,14 @@ export async function login(requestObject: Object) {
 //logout
 
 export async function logout(requestObject: any) {
-    
+
     let response;
     try {
 
         const endpoint = '/api/account/logout';
 
 
-         response = await fetch(BASE_API_URL + endpoint, {
+        response = await fetch(BASE_API_URL + endpoint, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
@@ -112,29 +112,28 @@ export async function logout(requestObject: any) {
         const data = await response.json();
 
         // Return the data
-        return {...data, response: "successful"};
+        return { ...data, response: "successful" };
 
     }
-    catch (error : any) {
+    catch (error: any) {
         // Log any errors to the console
         console.error("There has been a problem with your fetch operation:", error);
         const data = await response.json();
-        return { response: "failed", status: error.msg, data: data};
+        return { response: "failed", status: error.msg, data: data };
     }
 }
 
-export async function addInventoryLocation(requestObject: any, method: string)
-{
+export async function addInventoryLocation(requestObject: any, method: string) {
     let response;
     let token = requestObject.token
     //delete requestObject.token 
-    
+
     try {
 
         const endpoint = '/api/inventory/locations/';
 
 
-         response = await fetch(BASE_API_URL + endpoint, {
+        response = await fetch(BASE_API_URL + endpoint, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
@@ -152,29 +151,28 @@ export async function addInventoryLocation(requestObject: any, method: string)
         const data = await response.json();
 
         // Return the data
-        return {...data, response: "successful"};
+        return { ...data, response: "successful" };
 
     }
-    catch (error : any) {
+    catch (error: any) {
         // Log any errors to the console
         console.error("There has been a problem with your fetch operation:", error);
         const data = await response.json();
-        return { response: "failed", status: error.msg, data: data};
+        return { response: "failed", status: error.msg, data: data };
     }
 }
 
-export async function editInventoryTab(requestObject: any, method: string)
-{
+export async function editInventoryTab(requestObject: any, method: string) {
     let response;
     let token = requestObject.token
-   // delete requestObject.token 
-    
+    // delete requestObject.token 
+
     try {
 
         const endpoint = '/api/inventory/locations/';
 
 
-         response = await fetch(BASE_API_URL + endpoint, {
+        response = await fetch(BASE_API_URL + endpoint, {
             method: 'PUT',
             headers: {
                 'Content-Type': 'application/json',
@@ -192,36 +190,35 @@ export async function editInventoryTab(requestObject: any, method: string)
         const data = await response.json();
 
         // Return the data
-        return {...data, response: "successful"};
+        return { ...data, response: "successful" };
 
     }
-    catch (error : any) {
+    catch (error: any) {
         // Log any errors to the console
         console.error("There has been a problem with your fetch operation:", error);
         const data = await response.json();
-        return { response: "failed", status: error.msg, data: data};
+        return { response: "failed", status: error.msg, data: data };
     }
 }
 
-export async function getInventoryLocations(requestObject: any, method: string)
-{
+export async function getInventoryLocations(requestObject: any, method: string) {
     let response;
     let token = requestObject.token
-   // delete requestObject.token 
-    
+    // delete requestObject.token 
+
     try {
 
         const endpoint = '/api/inventory/locations/';
 
 
-         response = await fetch(BASE_API_URL + endpoint, {
+        response = await fetch(BASE_API_URL + endpoint, {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
                 'Authorization': `Token ${token}`
             },
             //  credentials: "include",
-            
+
         });
 
 
@@ -232,27 +229,26 @@ export async function getInventoryLocations(requestObject: any, method: string)
         const data = await response.json();
 
         // Return the data
-        return {...data, response: "successful"};
+        return { ...data, response: "successful" };
 
     }
-    catch (error : any) {
+    catch (error: any) {
         // Log any errors to the console
         console.error("There has been a problem with your fetch operation:", error);
         const data = await response.json();
-        return { response: "failed", status: error.msg, data: data};
+        return { response: "failed", status: error.msg, data: data };
     }
 }
 // Inventory APIs
 
-export async function addInventory(requestObject: any, method: string)
-{
+export async function addInventory(requestObject: any, method: string) {
     let response;
     let token = requestObject.token
-   // delete requestObject.token 
+    // delete requestObject.token 
     try {
 
         const endpoint = '/api/inventory/me/';
-         response = await fetch(BASE_API_URL + endpoint, {
+        response = await fetch(BASE_API_URL + endpoint, {
             method: method,
             headers: {
                 'Content-Type': 'application/json',
@@ -260,7 +256,7 @@ export async function addInventory(requestObject: any, method: string)
             },
             body: JSON.stringify(requestObject)
             //  credentials: "include",
-           
+
         });
 
 
@@ -271,33 +267,32 @@ export async function addInventory(requestObject: any, method: string)
         const data = await response.json();
 
         // Return the data
-        return {...data, response: "successful"};
+        return { ...data, response: "successful" };
 
     }
-    catch (error : any) {
+    catch (error: any) {
         // Log any errors to the console
         console.error("There has been a problem with your fetch operation:", error);
         const data = await response.json();
-        return { response: "failed", status: error.msg, data: data};
+        return { response: "failed", status: error.msg, data: data };
     }
 }
 
 
-export async function getInventory(requestObject: any)
-{
+export async function getInventory(requestObject: any) {
     let response;
     try {
 
         const endpoint = '/api/inventory/me/';
 
-         response = await fetch(BASE_API_URL + endpoint, {
+        response = await fetch(BASE_API_URL + endpoint, {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
                 'Authorization': `Token ${requestObject.token}`
             },
             //  credentials: "include",
-           
+
         });
 
 
@@ -308,28 +303,28 @@ export async function getInventory(requestObject: any)
         const data = await response.json();
 
         // Return the data
-        return {...data, response: "successful"};
+        return { ...data, response: "successful" };
 
     }
-    catch (error : any) {
+    catch (error: any) {
         // Log any errors to the console
         console.error("There has been a problem with your fetch operation:", error);
         const data = await response.json();
-        return { response: "failed", status: error.msg, data: data};
+        return { response: "failed", status: error.msg, data: data };
     }
 }
 
 /* Function for inventory page  */
 
 
-export async function addEditItems(requestObject: Object,token: String, action: string) {
-    
+export async function addEditItems(requestObject: Object, token: String, action: string) {
+
     let response;
     try {
 
         const endpoint = '/api/inventory/items/';
         let method;
-        switch (action){
+        switch (action) {
             case 'add':
                 method = 'POST';
                 break;
@@ -340,7 +335,7 @@ export async function addEditItems(requestObject: Object,token: String, action: 
                 method = 'DELETE'
                 break;
         }
-         response = await fetch(BASE_API_URL + endpoint, {
+        response = await fetch(BASE_API_URL + endpoint, {
             method: method,
             headers: {
                 'Content-Type': 'application/json',
@@ -358,14 +353,56 @@ export async function addEditItems(requestObject: Object,token: String, action: 
         const data = await response.json();
 
         // Return the data
-        return {...data, response: "successful"};
+        return { ...data, response: "successful" };
 
     }
-    catch (error : any) {
+    catch (error: any) {
         // Log any errors to the console
         console.error("There has been a problem with your fetch operation:", error);
         const data = await response.json();
-        return { response: "failed", status: error.msg, data: data};
+        return { response: "failed", status: error.msg, data: data };
+    }
+}
+
+export async function fetchShoppingList(requestObject: any, token: String, method: string) {
+    let response;
+    //delete requestObject.token 
+
+    try {
+
+        const endpoint = '/api/inventory/shopping-lists/';
+        const options = {
+            method: method,
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Token ${token}`
+            }
+
+        };
+        if (method != "GET") 
+        { 
+            options.body = JSON.stringify(requestObject); 
+        }
+
+
+        response = await fetch(BASE_API_URL + endpoint, options);
+
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        // Parse the JSON response
+        const data = await response.json();
+
+        // Return the data
+        return { ...data, response: "successful" };
+
+    }
+    catch (error: any) {
+        // Log any errors to the console
+        console.error("There has been a problem with your fetch operation:", error);
+        const data = await response.json();
+        return { response: "failed", status: error.msg, data: data };
     }
 }
 
@@ -374,22 +411,22 @@ export async function addEditItems(requestObject: Object,token: String, action: 
 export function concatenateArraysAndJoin(obj: object) {
 
     let combinedArray: string[] = [];
-  
-  
+
+
     for (const key in obj) {
-  
-      if (obj.hasOwnProperty(key)) {
-  
-        if (Array.isArray(obj[key])) {
-  
-          combinedArray = combinedArray.concat(obj[key]);
+
+        if (obj.hasOwnProperty(key)) {
+
+            if (Array.isArray(obj[key])) {
+
+                combinedArray = combinedArray.concat(obj[key]);
+            }
         }
-      }
     }
-  
+
     // Join the elements of the combinedArray into a string, separated by commas
     const resultString = combinedArray.join(',');
-  
+
     // Return the resulting string
     return resultString;
-  }
+}
--- src/pages/shopping/ShoppingPage.tsx
+++ src/pages/shopping/ShoppingPage.tsx
@@ -9,8 +9,8 @@ import {
 } from "react-router-dom";
 import { Route, Redirect } from 'react-router';
 
-import { getInventory, logout, concatenateArraysAndJoin } from '../../helper/APIRequest';
-import { useState, useReducer } from 'react';
+import { getInventory, logout,fetchShoppingList, concatenateArraysAndJoin } from '../../helper/APIRequest';
+import { useState, useEffect,useReducer } from 'react';
 
 
 import './ShoppingPage.css';
@@ -33,13 +33,13 @@ export interface ShoppingListItemModel {
   unit: string;
   id?:  number;
   crossed?: boolean,
-  isComplete? : boolean
+  is_complete? : boolean
 }
 
 export interface ShoppingListModel {
   name: string;
-  starred: boolean;
-  isComplete: boolean;
+  is_favorite: boolean;
+  is_complete: boolean;
   id?: string | number;
   itemList?: ShoppingListItemModel[]
 }
@@ -56,8 +56,8 @@ let fixedShoppingListItems: ShoppingListModel[] = [
   {
     id: 1,
     name: "Frys List",
-    starred: false,
-    isComplete: false,
+    is_favorite: false,
+    is_complete: false,
     itemList: [
       {  
         name: "Lettuce",
@@ -83,8 +83,8 @@ let fixedShoppingListItems: ShoppingListModel[] = [
   {
     id: 2,
     name: "Safeway List",
-    starred: false,
-    isComplete: false,
+    is_favorite: false,
+    is_complete: false,
     itemList: [ {  
       name: "milk",
       quantity: 2,
@@ -95,8 +95,8 @@ let fixedShoppingListItems: ShoppingListModel[] = [
   {
     id: 3,
     name: "Running list",
-    starred: false,
-    isComplete: false,
+    is_favorite: false,
+    is_complete: false,
     itemList: []
   }
 ]
@@ -107,8 +107,8 @@ function shoppingListReducer(lists, action) {
       return [...lists, {
         id: action.id,
         name: action.name,
-        starred: action.starred,
-        isComplete: action.isComplete,
+        is_favorite: action.is_favorite,
+        is_complete: action.is_complete,
         itemList: []
       }];
     }
@@ -120,8 +120,14 @@ function shoppingListReducer(lists, action) {
  
     case 'favorite': {
       return lists.map(t =>
-        t.id === action.id ? { ...t, starred: action.starred } : t
+        t.id === action.id ? { ...t, is_favorite: action.is_favorite } : t
       )
+    }
+
+    case 'fetch-lists': {
+      return action.itemList.map(t =>
+         ({ ...t, id: t.list_id })
+      );
     }
     case 'add-item': {
       // let newitem: ActionModel = {
@@ -177,6 +183,9 @@ function ShoppingPageMain(props: any) {
 
   let shoppingLists = props.shoppingLists;
   let dispatch = props.dispatch;
+
+
+
   return (
     //{this.state.shoppingListItems.map((itemName, category, quantity,))}
     // <IonPage>    
@@ -199,7 +208,7 @@ function ShoppingPageMain(props: any) {
 
               <IonIcon slot="icon-only" icon={starSharp} id={"star-shoppinglist-" + item.id} className="star-item" onClick={() => {
                 //Add API call
-                dispatch({ type: 'favorite', id: item.id, starred: !item.starred });
+                dispatch({ type: 'favorite', id: item.id, is_favorite: !item.is_favorite });
               }} />
 
 
@@ -227,8 +236,8 @@ function ShoppingPageMain(props: any) {
             type: 'create',
             id: 11,
             name: "",
-            starred: false,
-            isComplete: false,
+            is_favorite: false,
+            is_complete: false,
             itemList: []
           }
           dispatch(newitem);
@@ -253,13 +262,72 @@ function ShoppingPageMain(props: any) {
 const ShoppingPage = (props: any) => {
   const [errorToast] = useIonToast();
   const [messageToast] = useIonToast();
+  const {showLoading: showLoading, hideLoading: hideLoading, token: token} = props
 
-  //const [shoppingListItems, setshoppingListItems ] = useState(fixedShoppingListItems); 
+
+  
 
   const [lists, dispatch] = useReducer(
     shoppingListReducer,
     fixedShoppingListItems
   );
+
+  useEffect(()=> {
+    //fetch inventory id and items if not found
+    // TODO:
+    const showL = async () => {
+      await showLoading();
+    }
+    
+    
+    // showL();
+    // fetchShoppingList({},props.token, "GET").then((resp) => {
+    //   if (resp.response == "failed") {
+
+
+    //     // merge together all response error message for a toast message.
+    //     const msg = concatenateArraysAndJoin(resp.data);
+
+    //     errorToast({
+    //       message: resp.detail,
+    //       duration: 1500,
+    //       position: "top",
+    //       color: "warning"
+    //     });
+
+    //   }
+      
+      
+    //    else if (resp.response == "successful") {
+    //     let list = Object.keys(resp).map(function(k) { return  resp[k] });
+    //     list.pop();
+    //    dispatch({itemList: list, type: 'fetch-lists'});
+
+    //   //   messageToast({
+    //   //     message: msg,
+    //   //     duration: 1500,
+    //   //     position: "top",
+    //   //     color: "success"
+    //   //   });
+    //    }
+    //    hideLoading();
+
+    // })
+    // .catch((err) => {
+    //   const msg = concatenateArraysAndJoin(err.data);
+
+    //   errorToast({
+    //     message: msg,
+    //     duration: 1500,
+    //     position: "top",
+    //     color: "warning"
+    //   });
+    //   hideLoading();
+
+    // });
+
+  }, [props.token])
+
 
   let history = props.history;
 
